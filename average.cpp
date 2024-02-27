9b4c7411b69a574a95b41a9f53e7b19b641fe3e6
Akkarapong Garpaya <112610820+Akkarapo@users.noreply.github.com>
2024-02-27 14:32:02 +0700

    Complete average

--- average.cpp
+++ average.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+double sum = 0;
+double numbers;
+double Average;
+
+int main(int argc,char * argv[]){
+    if(argc <= 1){
+        cout << "Please input numbers to find average.";
+    }
+    else{
+        for(int i=0; i<argc;i++){
+            numbers = atof(argv[i]);
+            sum += numbers;
+            if(i == argc-1){
+                Average = sum/i;
+                cout << "---------------------------------" << endl;
+                cout << "Average of "<< i <<" numbers = " << Average << endl;
+                cout << "---------------------------------" << endl;
+            }
+        }
+    }
+    return 0;
+
+}
